89862b16b9542c8b0d674dc24bb797515129c35a
Joshua Gardner <joshgardner2000@gmail.com>
2024-02-21 21:07:40 +1100

    Add EDPL ics calendars page and calendar

---
+++ src/routes/edpl/+page.svelte
@@ -0,0 +1,17 @@
+<h1>EDPL Fixture Calendars</h1>
+<h2>Summer 2023/24</h2>
+<a href="/assets/edpl/3-3-2023.ics" download>Manhattan 4ths</a>
+
+<style lang="css">
+    @font-face {
+        font-family: VulfSans;
+        src:
+            URL('/assets/fonts/Vulf_Sans-Regular.woff') format('woff'),
+            /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+                URL('/assets/fonts/Vulf_Sans-Regular.ttf') format('truetype');
+        /* Chrome 4+, Firefox 3.5, Opera 10+, Safari 3—5 */
+    }
+    * {
+        font-family: VulfSans;
+    }
+</style>
