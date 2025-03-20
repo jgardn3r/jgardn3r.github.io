5b0c0d9ee7cb6b00fcd5b3a97cc022bbbe108eed
Joshua Gardner <joshgardner2000@gmail.com>
2025-03-20 07:34:04 +0000

    Updated %DATE% %TIME%

---
+++ _app/immutable/nodes/5.9efceb8a.js
@@ -0,0 +1,5 @@
+import{S as p,i as d,s as h,q as m,k as _,r as u,l as x,m as S,h as i,b as f,G as b,N as g,H as r}from"../chunks/index.1e6c574a.js";function v(n){let s,t,o,l,c;return{c(){s=m(`here is a test page
+
+`),t=_("button"),o=m("Download CSV")},l(e){s=u(e,`here is a test page
+
+`),t=x(e,"BUTTON",{});var a=S(t);o=u(a,"Download CSV"),a.forEach(i)},m(e,a){f(e,s,a),f(e,t,a),b(t,o),l||(c=g(t,"click",w),l=!0)},p:r,i:r,o:r,d(e){e&&i(s),e&&i(t),l=!1,c()}}}function w(){fetch("/assets/edpl/4-2-2024-Summer.ics").then(n=>n.text()).then(n=>console.log(n))}function C(n){return[]}class k extends p{constructor(s){super(),d(this,s,C,v,h,{})}}export{k as component};
